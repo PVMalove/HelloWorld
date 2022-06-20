c4c324b074f78edb6f9cd4baabc2a328f0aa70fa
MalovePi <p.malove@yandex.ru>
2022-06-20 20:22:15 +0300

    Lesson 2

--- HelloWorld/OTUS_Modul_03/Program.cs
+++ HelloWorld/OTUS_Modul_03/Program.cs
@@ -9,8 +9,26 @@ namespace OTUS_Modul_03
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Console.ReadLine();
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
 
+            Drow(x1, y1, c1);
+
+            int x2 = 10;
+            int y2 = 4;
+            char c2 = '#';
+
+            Drow(x2, y2, c2);
+
+                                    
+            Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
