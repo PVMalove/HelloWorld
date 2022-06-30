a2ed8b60de2bbb23f68c8d5b24cf75c1d1a141e7
MalovePi <p.malove@yandex.ru>
2022-06-30 18:40:01 +0300

    Lesson 18
    Реализация функции таймера.
    Устранение бага Race Conditions.

--- HelloWorld/OTUS_Modul_03/Tetris.cs
+++ HelloWorld/OTUS_Modul_03/Tetris.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Threading;
+using System.Timers;
 
 namespace OTUS_Modul_03
 {
     class Tetris
     {
+        const int TIMER_INTERVL = 500;
+        static System.Timers.Timer timer;
+
+        private static Object _lockObject = new object(); 
+        
+        static Figure currentFigure;
         static FigureGenerator generator;
         static void Main()
         {
@@ -13,18 +20,21 @@ namespace OTUS_Modul_03
             Console.SetBufferSize(GameField.WindowWidth, GameField.WindowHeight);
                                               
             generator = new FigureGenerator(GameField.WindowWidth/2, 0, CellsRenderer.DEFOULT_SYMBOL);
-            Figure currentFigure = generator.GetNewFigure();
+            currentFigure = generator.GetNewFigure();
+            SetTimer();
             while (true)
             {
                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
+                    Monitor.Enter(_lockObject);
                     ResultCollision resultHandleKey = HandleKey(currentFigure, key);
                     ProcessResult(resultHandleKey, ref currentFigure);
+                    Monitor.Exit(_lockObject);
                 }
             }
         }
-
+                
         private static bool ProcessResult(ResultCollision resultHandleKey, ref Figure currentFigure)
         {
             if (resultHandleKey == ResultCollision.FigureCollision || resultHandleKey == ResultCollision.DownBorderCollision)
@@ -52,6 +62,21 @@ namespace OTUS_Modul_03
                     return currentFigure.TryRotateFigure();
             }
             return ResultCollision.NoCollision;
+        }
+
+        private static void SetTimer()
+        {
+            timer = new System.Timers.Timer(TIMER_INTERVL);
+            timer.Elapsed += OnTimerEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+        private static void OnTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            Monitor.Enter(_lockObject);
+            ResultCollision resultMove = currentFigure.TryMoveFigure(DirectionMovementFigure.Down);
+            ProcessResult(resultMove, ref currentFigure);
+            Monitor.Exit(_lockObject);
         }
     }
 }
