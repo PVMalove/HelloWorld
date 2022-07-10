df96003301721e1e0452e41da6f76a74cf37a0c6
MalovePi <p.malove@yandex.ru>
2022-07-10 22:06:11 +0300

    Lesson 04
    Двухмерные массивы

--- HelloWorld/03_Arrays/Lesson.cs
+++ HelloWorld/03_Arrays/Lesson.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03_Arrays
 {
@@ -15,6 +11,8 @@ namespace _03_Arrays
             //ArraysPractice02();
             //ArraysPractice03();
             //Homework.HomeWork01();
+            //TwoDimensionalArrayLesson01();
+            TwoDimensionalArrayLesson02();
 
             Console.ReadKey();
         }
@@ -73,19 +71,19 @@ namespace _03_Arrays
                         int userTable, userPlace;
                         Console.Write("Столик для бронирования: ");
                         userTable = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if(tables.Length <= userTable || userTable < 0 )
+                        if (tables.Length <= userTable || userTable < 0)
                         {
                             Console.WriteLine("Такого стала нет!");
                             break;
                         }
                         Console.Write("Количество мест для бронирования: ");
                         userPlace = Convert.ToInt32(Console.ReadLine());
-                        if(tables[userTable]< userPlace)
+                        if (tables[userTable] < userPlace)
                         {
                             Console.WriteLine("Не достаточно мест!");
                             break;
                         }
-                        else if(userPlace <= 0)
+                        else if (userPlace <= 0)
                         {
                             Console.WriteLine("Не коректное значение!");
                             break;
@@ -99,6 +97,33 @@ namespace _03_Arrays
                 }
                 Console.ReadKey();
                 Console.Clear();
+            }
+        }
+        static void TwoDimensionalArrayLesson01()
+        
+        {
+            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void TwoDimensionalArrayLesson02()
+        {
+            int[,] array = new int[6, 6];
+            Random rand = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand.Next(0, 2);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
