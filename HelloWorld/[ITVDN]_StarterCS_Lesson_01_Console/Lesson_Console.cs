af51e0a5b3fab5b55a8b9e7ea29c915b8af3f12c
MalovePi <p.malove@yandex.ru>
2022-08-02 18:39:38 +0300

    Арифметические операторы

--- HelloWorld/[ITVDN]_StarterCS_Lesson_01_Console/Lesson_Console.cs
+++ HelloWorld/[ITVDN]_StarterCS_Lesson_01_Console/Lesson_Console.cs
@@ -15,6 +15,10 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             //ExplicitCasting();
             //NumericOperations();
             //AverageTemperature();
+            //ConsoleColors();
+            //NotANumber();
+            //CompoundAssignment();
+            Priority();
             Console.ReadKey();
         }
         static void MusicalScalePractice01()
@@ -102,9 +106,9 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             Console.WriteLine();
 
             //ТЕСТИРОВАНИЕ
-            T_mo = 1; T_tu = 1; T_we = 1; T_th = 1; T_fr = 1; T_sa = 1; T_su = 1; 
+            T_mo = 1; T_tu = 1; T_we = 1; T_th = 1; T_fr = 1; T_sa = 1; T_su = 1;
             daysInWeek = 7;
-                                       // место ошибки
+            // место ошибки
             T_sum = T_mo + T_tu + T_we - T_th + T_fr + T_sa + T_su; // == 7 Эталоное значение
             T_avg = T_sum / daysInWeek;                             // == 1 Эталоное значение
             Console.WriteLine("T_sum: " + T_sum + " == 7 Эталоное значение");
@@ -113,7 +117,76 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             T_sum = T_mo + T_tu + T_we + T_th + T_fr + T_sa + T_su; // == 7 Эталоное значение
             T_avg = T_sum / daysInWeek;                             // == 1 Эталоное значение
             Console.WriteLine("T_sum: " + T_sum + " == 7 Эталоное значение");
-            Console.WriteLine("T_avg: " + T_avg + " == 1 Эталоное значение");            
+            Console.WriteLine("T_avg: " + T_avg + " == 1 Эталоное значение");
+        }
+        //Lesson 13 ArithmeticOperations
+        static void ConsoleColors()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello world!");
+            for (int i = 0; i < 14; i++)
+            {
+                Console.ForegroundColor++;
+                Console.WriteLine("Hello world!");
+            }
+        }
+        //Унарный плюс и минус (унарный оператор "-" измененяет знак операнда на противоположный)
+        //Not-a-Number «нечисло»
+        static void NotANumber()
+        {
+            //К операциям, приводящим к появлению NaN в качестве ответа, относятся:
+            //o все математические операции, содержащие NaN в качестве одного из операндов;
+            Console.WriteLine(2 + double.NaN);
+            //o деление нуля на ноль;
+            Console.WriteLine(-0d / +0d);
+            //o деление бесконечности на бесконечность;
+            Console.WriteLine(double.NegativeInfinity / double.PositiveInfinity);
+            //o умножение нуля на бесконечность;
+            Console.WriteLine(0 * double.PositiveInfinity);
+            //o сложение бесконечности с бесконечностью противоположного знака;
+            Console.WriteLine(double.PositiveInfinity + double.NegativeInfinity);
+            //o вычисление квадратного корня отрицательного числа;
+            Console.WriteLine(Math.Sqrt(-12));
+            //o логарифмирование отрицательного числа.
+            Console.WriteLine(Math.Log(-10, 5));
+            // Пример с возвидением в степень, для получения числа
+            Console.WriteLine(Math.Pow(11, 2));
+        }
+        //Составное присваивание
+        static void CompoundAssignment()
+        {
+            int a = 5;
+            a += 9;
+            Console.WriteLine(a);
+            a -= 4;
+            Console.WriteLine(a);
+            a *= 2;
+            Console.WriteLine(a);
+            a /= 4;
+            Console.WriteLine(a);
+            a %= 3;
+            Console.WriteLine(a);
+        }
+        //Приоритет и ассоциативность операторов:
+        //1) Префиксный инкремент ++x и декремент --x, унарные операторы + и -
+        //2) Мультипликативные операторы *, /, и %
+        //3) Аддитивные операторы + и -
+        //4) Постфиксный инкремент x++ и декремент x--
+        static void Priority()
+        {
+            //Посфиксный инкремент: х++
+            int a = 0, b = a++ * 5;
+            Console.WriteLine($"a = {a}, b = {b}");
+            int c = 0, d = c++ + 5;
+            Console.WriteLine($"c = {c}, d = {d}");
+
+            //Префиксный инкремент: ++х
+            int e = 0, f = ++e * 5;
+            Console.WriteLine($"e = {e}, f = {f}");
+            int g = 0, h = ++g + 5;
+            Console.WriteLine($"g = {g}, h = {h}");
+
         }
     }
 }
