3bbd52d7c198d2b24155828da60be662ccdc4358
MalovePi <p.malove@yandex.ru>
2022-08-07 21:40:50 +0300

    Операции над строковыми переменными часть 3

--- HelloWorld/[ITVDN]_StarterCS_Lesson_01_Console/Lesson_Console.cs
+++ HelloWorld/[ITVDN]_StarterCS_Lesson_01_Console/Lesson_Console.cs
@@ -27,6 +27,8 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             //Calculator();
             //FormttedOutput();
             //FormatSpecifiers();
+            //Interpolatio();
+            //EscapeSequences();
             #endregion
             #region Lesson 16
             #endregion
@@ -53,8 +55,7 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             double j = -1.12345d;
             decimal k = -1.12345m;
         }
-        #region Lesson 12
-        //Lesson 12 Операции над числовыми переменными
+        #region Lesson 12 Operations on numerical variables - Операции над числовыми переменными
         static void NumericOperations()
         {
             //Члены-участники арифметических действий:
@@ -113,8 +114,7 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             Console.WriteLine("T_avg: " + T_avg + " == 1 Эталоное значение");
         }
         #endregion
-        #region Lesson 13
-        //Lesson 13 Arithmetic operations - Арифметические операторы
+        #region Lesson 13 Arithmetic operations - Арифметические операторы
         static void ConsoleColors()
         {
             Console.BackgroundColor = ConsoleColor.White;
@@ -181,8 +181,7 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             Console.WriteLine($"g = {g}, h = {h}");
         }
         #endregion
-        #region Lesson 14
-        //Lesson14 String variables - Строковые переменные
+        #region Lesson 14 String variables - Строковые переменные
         static void StringVariables()
         {                     //Стараковой литирал
             string sentence = "Hello world!";
@@ -204,8 +203,7 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             Console.WriteLine(sentence);
         }
         #endregion
-        #region Lesson 15
-        //Lesson 15 Operations on string variables - Операции над строковыми переменными       
+        #region Lesson 15 Operations on string variables - Операции над строковыми переменными       
         static void Concatenation() //Конкатенация строк
         {
             //Конкатенация строк (объединение строк)
@@ -240,7 +238,7 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             string result = augent + " + " + addend + " = " + sum;
             Console.WriteLine(result);
         }
-        static void FormttedOutput() //Форматированный вывод
+        static void FormttedOutput() //Форматированный вывод - Строка составного формата
         {
             Console.Write("Введите первое слагаемое: ");
             string stringAugent = Console.ReadLine();
@@ -253,15 +251,23 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
 
             int sum = augent + addend;
                             //Строка составного формата
-            string result = string.Format("Результат: {0} + {1} = {2}", augent, addend, sum);
+            string result = string.Format("Результат: {0} + {1} = {2}", augent, addend, sum);            
+            /* Строка составного формата может быть указана только в параметрах метода. 
+               Присвоить ее переменной напрямую не получится. */
+
+            /* У типа string есть метод с именем Format, которому можно в параметры передать
+               «строку составного формата» для получения готовой строки с подставленным 
+               в нужные места значениями. */
             Console.WriteLine(result);
         }
         static void FormatSpecifiers() //Флаги форматирования
         {
-            /*Маркер подстановки – это специальный оператор форматированного вывода,
-            позволяющий указать место в строке вывода, куда может быть вставлено 
-            значение некой переменной по ее идентификатору.*/
-            Console.WriteLine("1. C format: {0:C2}", 2.50);     // Валюта
+            //https://docs.microsoft.com/ru-ru/dotnet/standard/base-types/formatting-types
+            /* Флаги форматирования (или как их еще называют – «Описатели формата») – 
+               набор операторов, позволяющий настроить внешний вид выводимых на экран 
+               текстовых данных, согласно какому-то из существующих форматов представления 
+               данных, например: формат процентов, формат валюты и др.*/
+            Console.WriteLine("1. C format: {0:C2}", 2.50);    // Валюта
             Console.WriteLine("2. D format: {0:D}", 26);       // Десятичное число
             Console.WriteLine("3. E format: {0:E2}", 250000);  // Экспоненциальный
             Console.WriteLine("4. F format: {0:F2}", 2.5);     // С фиксированной запятой
@@ -270,6 +276,80 @@ namespace _ITVDN__StarterCS_Lesson_01_Console
             Console.WriteLine("7. P format: {0:P}", 0.25);     // Процент
             Console.WriteLine("8. R format: {0:R}", 2.5);      // Приемо-передача (Round-Trip)
             Console.WriteLine("9. X format: {0:X}", 250);      // Шестнадцатеричный
+        }
+        static void Interpolatio() //Интерполяция строк
+        {
+            int augent = 2;
+            int addend = 2;
+            int sum = augent + addend;
+            string result = $"Результат: {augent} + {addend} = {sum}";
+            Console.WriteLine(result);
+
+            Console.WriteLine($"1. C format: {2.5:C2}");       // Валюта
+            Console.WriteLine($"2. D format: {0xFF:D}");       // Десятичное число
+            Console.WriteLine($"3. E format: {250000:E2}");    // Экспоненциальный
+            Console.WriteLine($"4. F format: {2.5:F2}");       // С фиксированной запятой
+            Console.WriteLine($"5. G format: {2.5:G}");        // Общий
+            Console.WriteLine($"6. N format: {2500000:N}");    // Numeric
+            Console.WriteLine($"7. P format: {0.25:P}");       // Процент
+            Console.WriteLine($"9. X format: {255:X}");        // Шестнадцатеричный
+
+            double interpolationExpression = 0.123456789_12345678;
+            const int aligment = 30;
+            /* Константа (Constant) – это область памяти, которая хранит в себе некоторое значение, которое нельзя изменить.
+               Правила использования констант:
+                o Константам необходимо присваивать значение непосредственно в месте создания;
+                o Попытка присвоения константе нового значения приводит к ошибке уровня компиляции; */
+            Console.WriteLine($"8. R format|{interpolationExpression,-aligment:R}| End.");  // Round-Trip
+            Console.WriteLine($"8. R format|{interpolationExpression,aligment:R}| End.");    // Round-Trip
+            //https://docs.microsoft.com/ru-ru/dotnet/csharp/language-reference/tokens/interpolated
+        }
+        static void EscapeSequences() //Escape-последовательность
+        {
+            Console.WriteLine('\''); // Single quotation mark
+            Console.WriteLine("\'");
+            Console.WriteLine("'");
+
+            Console.WriteLine("Вася сказал: \"Привет\"."); // Double quotation mark
+            Console.WriteLine(@"Вася сказал: ""Привет"".");
+
+            Console.WriteLine("C:\\Windows\\notepad.exe"); // Путь к блокноту
+            Console.WriteLine(@"C:\Windows\notepad.exe");
+
+            Console.WriteLine("\a");           // Alert (Звук-предупреждение)
+
+            Console.Write("Backspace\b");      // Возврат на одну позицию
+            Console.WriteLine("!");
+
+            Console.Write("Hello\n");          // Переход на новую строку
+
+            Console.Write("Hello\r");          // Возврат (каретки) курсора в начало строки 
+
+            Console.WriteLine("1 \t 2 \t 3");  // Горизонтальная табуляция
+
+            // Коды ASCII символов в шестнадцатеричном формате
+            //                   H   e   l   l  o        w   o   r   l   d   !
+            Console.WriteLine("\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21");
+
+            // Коды Unicode символов в шестнадцатеричном формате
+            //                   H      e     l     l     o           w     o     r     l     d     !
+            Console.WriteLine("\x0048\x0065\x006c\x006c\x006f\x0020\x0057\x006f\x0072\x006c\x0064\x0021");
+
+            // Коды Unicode символов в шестнадцатеричном формате
+            //                   H      e     l     l     o           w     o     r     l     d     !
+            Console.WriteLine("\u0048\u0065\u006c\u006c\u006f\u0020\u0057\u006f\u0072\u006c\u0064\u0021");
+
+            // Коды ASCII символов в шестнадцатеричном формате
+            Console.WriteLine('\x48' + '\x65' + '\x6c' + '\x6c' + '\x6f' + '\x20' +
+                              '\x57' + '\x6f' + '\x72' + '\x6c' + '\x64' + '\x21');
+
+            // Коды Unicode символов в шестнадцатеричном формате
+            Console.WriteLine('\u0048' + '\u0065' + '\u006c' + '\u006c' + '\u006f' + '\u0020' +
+                              '\u0057' + '\u006f' + '\u0072' + '\u006c' + '\u0064' + '\u0021');
+
+            // Коды Unicode символов в шестнадцатеричном формате
+            Console.WriteLine('\u0048' + '\u0065' + '\u006c' + '\u006c' + '\u006f' + '\u0020' +
+                              '\u0057' + '\u006f' + '\u0072' + '\u006c' + '\u0064' + '\u0021');
         }
         #endregion
         #region Lesson 16
