20acbcf37d48ca4444d64cea8f46f9a52917cb3f
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2024-09-01 23:07:10 +0300

    Init project WebApi "Notes"

---
+++ HelloWorld/WebApi/CRUD_NOTE/DataAccess/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+// Это основной класс контекста базы данных, который управляет сущностями и их состоянием.
+// Он наследует от `DbContext`, что позволяет использовать функциональность Entity Framework для работы с базой данных.
+// Конструктор класса, который принимает параметры конфигурации для контекста базы данных.
+public class AppContext(DbContextOptions<AppContext> options) : DbContext(options)
+{
+    // Этот DbSet свойство представляет таблицу "Notes" в базе данных.
+    // Оно позволяет выполнять CRUD операции (Create, Read, Update, Delete) с записями в этой таблице.
+    public DbSet<Note> Notes { get; set; }
+    
+    // Метод, который настраивает модель.
+    // Здесь мы определяем конфигурацию для сущности Note.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Указываем, что свойство Id является первичным ключом для сущности Note.
+        modelBuilder.Entity<Note>().HasKey(x => x.Id);
+        // Ограничиваем длину текста в свойстве Text до 140 символов.
+        modelBuilder.Entity<Note>().Property(x => x.Text).HasMaxLength(140);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ HelloWorld/WebApi/CRUD_NOTE/DataAccess/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess;
+
+public static class Extensions
+{
+    // Этот метод расширения AddDataAccess добавляет необходимые сервисы для работы с базой данных в приложении.
+    public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Здесь мы добавляем сервис AppContext в контейнер зависимостей.
+        // AppContext является контекстом базы данных, который используется для взаимодействия с базой данных.
+        services.AddDbContext<AppContext>(x =>
+        {
+            // Здесь мы настраиваем параметры подключения к базе данных.
+            // В данном случае, мы используем PostgreSQL базу данных
+            x.UseNpgsql(configuration.GetConnectionString("DbConnection")); 
+        });
+
+        return services;
+    }
+}
